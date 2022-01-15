989476a3717ec4c5261d6a59c012f0633956174e
Zain Malik <zainmalik7345@gmail.com>
2022-01-16 03:33:42 +0500

    maybe last

--- certificate-2/lazy/project.py
+++ certificate-2/lazy/project.py
@@ -6,19 +6,24 @@ RoboCorp Certificate level 2
 from RPA.Browser.Selenium import Selenium
 from RPA.HTTP import HTTP
 from RPA.Tables import Tables
+from RPA.PDF import PDF
 
 import time
 from datetime import timedelta
 
 
 class Certificate:
-    def __init__(self, downloading_path, csv_path):
+    def __init__(self, downloading_path, screenshot_path, pdf_path, receipt_directory, csv_path):
 
         self.browser = Selenium()
         self.http = HTTP()
         self.table = Tables()
+        self.pdf = PDF()
 
         self.downloading_path = downloading_path
+        self.screenshot_path = screenshot_path
+        self.pdf_path = pdf_path
+        self.receipt_directory = receipt_directory
         self.csv_path = csv_path
 
     def download_directory(self):
@@ -57,16 +62,28 @@ class Certificate:
             self.browser.click_button_when_visible(locator="//*[contains(text(), 'Preview')]")
             while True:
                 try:
-                    self.browser.click_button_when_visible(locator="(//*[contains(text(), 'Order')])[2]")
                     self.browser.wait_until_page_contains_element(locator="(//*[contains(text(), 'Order')])[2]",
                                                                   timeout=timedelta(seconds=17))
-                    time.sleep(2.5)
-                    self.browser.click_button_when_visible(locator="//*[contains(text(), 'another')]")
-                    """
-                    Re-use the above click_ok method, advantages of using OOP
-                    """
-                    self.click_ok()
+                    self.browser.click_button_when_visible(locator="(//*[contains(text(), 'Order')])[2]")
+                    self.browser.wait_until_page_contains_element(locator="//div[@id='receipt']",
+                                                                  timeout=timedelta(seconds=17))
+
                     break
                 except Exception:
                     pass
+            time.sleep(1)
+            self.browser.screenshot(locator="//div[@id='robot-preview-image']",
+                                    filename=f"{self.screenshot_path}/{order['Order number']}.png")
+            receipt_html = self.browser.get_element_attribute(locator="//div[@id='receipt']", attribute='innerHTML')
+            self.pdf.html_to_pdf(receipt_html, f"{self.pdf_path}/{order['Order number']}.pdf")
+            self.pdf.add_watermark_image_to_pdf(image_path=f"{self.screenshot_path}/{order['Order number']}.png",
+                                                source_path=f"{self.pdf_path}/{order['Order number']}.pdf",
+                                                output_path=f"{self.receipt_directory}/{order['Order number']}.pdf")
+            # another order
+            self.browser.click_button_when_visible(locator="//*[contains(text(), 'another')]")
+            """
+            Re-use the above click_ok method, advantages of using OOP
+            """
+            self.click_ok()
+
         time.sleep(5)
--- certificate-2/task.py
+++ certificate-2/task.py
@@ -4,12 +4,31 @@ import os
 
 from lazy.project import Certificate
 
-path = F"{os.getcwd()}/output"
-orders_path = f"{os.getcwd()}/orders.csv"
+base_directory = os.getcwd()
+
+download_path = F"{base_directory}/output"
+
+screenshot_directory = f'{download_path}/screenshot'
+pdf_directory = f'{download_path}/pdf'
+receipt_direc = f"{download_path}/receipt"
+
+orders_path = f"{base_directory}/orders.csv"
+
+
+def paths():
+
+    if os.path.exists(screenshot_directory) or os.path.exists(pdf_directory) or os.path.exists(receipt_direc):
+        pass
+    else:
+        os.mkdir(screenshot_directory)
+        os.mkdir(pdf_directory)
+        os.mkdir(receipt_direc)
 
 
 def run_it():
-    certificate = Certificate(downloading_path=path, csv_path=orders_path)
+    certificate = Certificate(downloading_path=download_path, csv_path=orders_path,
+                              screenshot_path=screenshot_directory, pdf_path=pdf_directory,
+                              receipt_directory=receipt_direc)
     try:
         certificate.download_directory()
         certificate.open_browser()
@@ -26,6 +45,7 @@ def minimal_task():
 
 if __name__ == "__main__":
     try:
+        paths()
         run_it()
     except Exception as e:
         print(e)
